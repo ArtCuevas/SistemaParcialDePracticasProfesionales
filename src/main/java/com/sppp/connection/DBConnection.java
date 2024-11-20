b4f023fab2c867f208550c7c877929631b05f234
unknown <artcupe@gmail.com>
2024-11-20 11:48:24 -0600

    Estructura base del proyecto, connection BD y model

---
+++ src/main/java/com/sppp/connection/DBConnection.java
@@ -0,0 +1,47 @@
+package com.sppp.connection;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+
+        private static DBConnection instance;
+        private Connection connection;
+        private static String URL ;
+        private static  String USER;
+        private static  String PASSWORD;
+
+        private DBConnection()throws SQLException {
+            try{
+                Properties properties = new Properties();
+                FileInputStream input = new FileInputStream("config.properties");
+                properties.load(input);
+                URL = properties.getProperty("DB_URL");
+                USER = properties.getProperty("DB_USER");
+                PASSWORD = properties.getProperty("DB_PASSWORD");
+                input.close();
+                this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static DBConnection getInstance() throws SQLException{
+            if(instance==null || instance.getConnection().isClosed()){
+                instance = new DBConnection();
+            }
+            return instance;
+        }
+
+        public Connection getConnection() {
+            return connection;
+        }
+}
---
+++ src/main/java/com/sppp/model/Student.java
@@ -0,0 +1,70 @@
+package com.sppp.model;
+
+public class Student {
+    private int idstudent;
+    private String name;
+    private String lastname;
+    private String nrc;
+    private String enrolment;
+    private Project idproject;
+
+    public Student() {
+    }
+
+    public Student(int idstudent, String name, String lastname, String nrc, String enrolment, Project idproject) {
+        this.idstudent = idstudent;
+        this.name = name;
+        this.lastname = lastname;
+        this.nrc = nrc;
+        this.enrolment = enrolment;
+        this.idproject = idproject;
+    }
+
+    public int getIdstudent() {
+        return idstudent;
+    }
+
+    public void setIdstudent(int idstudent) {
+        this.idstudent = idstudent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getNrc() {
+        return nrc;
+    }
+
+    public void setNrc(String nrc) {
+        this.nrc = nrc;
+    }
+
+    public String getEnrolment() {
+        return enrolment;
+    }
+
+    public void setEnrolment(String enrolment) {
+        this.enrolment = enrolment;
+    }
+
+    public Project getIdproject() {
+        return idproject;
+    }
+
+    public void setIdproject(Project idproject) {
+        this.idproject = idproject;
+    }
+}
