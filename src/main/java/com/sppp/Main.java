c93a2137bd284b86a98716bb8a78bfeb04085f24
Erick <erickmelvzlpz@gmail.com>
2024-11-25 08:43:29 -0600

    Prueba erick

--- src/main/java/com/sppp/Main.java
+++ src/main/java/com/sppp/Main.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        //pruebz comentsrio
         SwingUtilities.invokeLater(() -> {
             Login loginForm = new Login();
             loginForm.setVisible(true);
